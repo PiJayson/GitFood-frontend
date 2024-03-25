4e5b3fdb6e78504a8a61e972955c849ffab3cda6
PiJayson <pijay888@gmail.com>
2024-03-25 11:37:37 +0100

    added default barcode scanning

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-import Quagga from 'quagga'; // ES6
-const Quagga = require('quagga').default; // Common JS (important: default)
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <View style={styles.container}><Text>Requesting for camera permission</Text></View>;
+  }
+  if (hasPermission === false) {
+    return <View style={styles.container}><Text>No access to camera</Text></View>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        style={styles.camera}
+        type={type}
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}>
+      </Camera>
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+      <Button title={'Flip Camera'} onPress={() => {
+        setType(
+          type === Camera.Constants.Type.back
+            ? Camera.Constants.Type.front
+            : Camera.Constants.Type.back
+        );
+      }} />
     </View>
   );
 }
@@ -16,8 +48,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
   },
+  camera: {
+    flex: 1,
+  }
 });
